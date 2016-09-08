f2e410513ad80f4d1b7aa33eee192bb716e1230a
Tobias Schmitt <tobias.schmitt@mni.thm.de>
2016-09-09 00:53:14 +0200

    design restructure
    changed design framework to materialize
    some program logic with cards in app.js

--- app.js
+++ app.js
@@ -40,29 +40,62 @@ function MQTTconnect() {
         options.userName = username;
         options.password = password;
     }
-    console.log("Host="+ host + ", port=" + port + ", path=" + path + " TLS = " + useTLS + " username=" + username + " password=" + password);
     mqtt.connect(options);
 }
 function onConnect() {
-    console.log("connected");
     //$('#status').val('Connected to ' + host + ':' + port + path);
     // Connection succeeded; subscribe to our topic
+    $('#conn_icon').html("signal_wifi_4_bar");
     mqtt.subscribe(topic, {qos: 0});
     //$('#topic').val(topic);
 }
 function onConnectionLost(response) {
     //setTimeout(MQTTconnect, reconnectTimeout);
+    $('#conn_icon').html("signal_wifi_off");
     //$('#status').val("connection lost: " + responseObject.errorMessage + ". Reconnecting");
 };
 function onMessageArrived(message) {
     var topic = message.destinationName;
     var payload = message.payloadString;
-    if(message.destinationName === "llearnd/machine/status") {
-        $('#wmstatus').html(message.payloadString);
+    if(message.destinationName === "llearnd/device/mState") {
+        var mState = parseInt(message.payloadString);
+        if(mState < 2) {
+            if($("#wmstatcard").hasClass("red")) {
+                $("#wmstatcard").removeClass("red");
+                $("#wmstatcard").addClass("green");
+            }
+            if(!$("#wmtimecardbox").hasClass("hide")) {
+                $("#wmtimecardbox").addClass("hide");
+            }
+            if(mState > 0) {
+                $("#wmstatus").html("... ist angeschaltet.");
+            } else {
+                $("#wmstatus").html("... ist ausgeschaltet.");
+            }
+        } else {
+            if($("#wmstatcard").hasClass("green")) {
+                $("#wmstatcard").removeClass("green");
+                $("#wmstatcard").addClass("red");
+            }
+            if($("#wmtimecardbox").hasClass("hide")) {
+                $("#wmtimecardbox").removeClass("hide");
+            }
+            $("#wmstatus").html("... läuft.");
+        }
     } else if (message.destinationName === "llearnd/device/time") {
         var d = new Date(parseInt(message.payloadString) * 1000);
         var timestr = d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
         $('#time').html(timestr);
+    } else if (message.destinationName === "llearnd/machine/lastBegin") {
+        var d = new Date(parseInt(message.payloadString) * 1000);
+        var timestr = d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+        $('#starttime').html(timestr);
+    } else if (message.destinationName === "llearnd/status") {
+        if(message.payloadString === "online") {
+            $('#daemon_icon').html("directions_run");
+        } else {
+            $('#daemon_icon').html("hotel");
+        }
     }
     //$('#ws').prepend('<li>' + topic + ' = ' + payload + '</li>');
 };
