5f6eb1c024a1eb6b5e909a55dd17536cc12ca976
Tobias Schmitt <tschmittldk@gmail.com>
2016-11-08 16:29:04 +0100

    Endzeiten mit Laundry learn

--- app.js
+++ app.js
@@ -97,12 +97,20 @@ function onMessageArrived(message) {
     } else if (message.destinationName === "llearnd/machine/lastBegin") {
         var d = new Date(parseInt(message.payloadString) * 1000);
         var timestr = d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-        $('#startTime').html(timestr);
+        $('.startTime').html(timestr);
     } else if (message.destinationName === "llearnd/state") {
         if(message.payloadString === "online") {
             $('#connIcon').html("signal_wifi_4_bar");
         } else {
             $('#connIcon').html("signal_wifi_4_bar_lock");
+        }
+    } else if (message.destinationName === "llearnd/learn/text") {
+        if(message.payloadString === "rechnen...") {
+            $('.endTime').html(message.payloadString);
+        } else {
+            var d = new Date(parseInt(message.payloadString) * 1000);
+            var timestr = d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+            $('.endTime').html(timestr);
         }
     } else if (message.destinationName === "llearnd/device/leds") {
         var leds = message.payloadString.split(',');
